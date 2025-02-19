b11857d2a55725493b20213665fd6440d67ff43c
mmakhlouf03 <166355390+mmakhlouf03@users.noreply.github.com>
2025-02-19 01:49:32 -0500

    Update cnn.py

--- xrd_ml/cnn.py
+++ xrd_ml/cnn.py
@@ -53,7 +53,7 @@ class XRDNet:
         """
         # Compile model
         self.model.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=5e-7), #optimizer='adam',
+            optimizer='adam', #optimizer=tf.keras.optimizers.Adam(learning_rate=5e-7),
             loss='mse',
             metrics=['mae', 'mse']
         )
