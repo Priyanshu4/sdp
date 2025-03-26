936fd9eafe0805d44e9c048bee860799cbaa83d8
Priyanshu Agrawal <priyanshu.agrawal@uconn.edu>
2025-03-25 23:35:42 -0400

    Update load_data_job.sh

--- hpc_jobs/load_data_job.sh
+++ hpc_jobs/load_data_job.sh
@@ -2,10 +2,11 @@
 
 #SBATCH -N 1
 #SBATCH -n 1
+#SBATCH -o load_data_%J.out
 
 module purge
 
-source /gpfs/homefs1/pra20003/miniconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate xrd_ml
 
